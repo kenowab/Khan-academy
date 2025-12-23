a2c4dca2ad862a6a3653745fc9950736e43bf432
Kenoweb <kenogud734@gmail.com>
2025-12-23 13:50:36 +0300

    i did navbar and hero section

---
+++ fortend/code.js
@@ -0,0 +1,133 @@
+
+console.log("this simple to code js");
+
+console.log("keno too");
+console.log(3 + 4);
+
+var k = 1000;
+let r = "keno too";
+const b = 60;
+var p = 50;
+let l = 60;
+
+console.log(k);
+console.log(p);
+
+let age = 23;
+console.log("I am " + age + " years old");
+
+console.log(2 / 3);
+
+let num1 = 40;
+let num2 = 50;
+let first_Name = "keno";
+let last_name = "gud";
+let full_name = first_Name + " " + last_name;
+
+console.log(full_name);
+console.log("Sum: " + (num1 + num2));
+console.log("Division: " + (num1 / num2));
+num1==num2;
+console.log(num1==num2);
+console.log(num1!=num2);
+console.log(num1>num2);
+console.log(num1<num2);
+console.log(num1>=num2);
+console.log(num1<=num2);
+var h=20;
+c="kiya";
+console.log(h+c);
+var myarray= [10,"keno","gud"];
+
+console.log(myarray[2]);
+function myfunction() {
+    console.log("this is my function");
+console.log("keno too");
+console.log(3 + 4);
+console.log("this is my function");
+}
+myfunction();
+
+let a=10;
+var n=20;
+console.log(a);
+function scopeTest() {
+    var x = 30;
+    let y = 40;
+    console.log(x); 
+    console.log(y); 
+
+}
+console.log(a);
+scopeTest();
+
+var  k=40;
+var t=50;
+if(k<t ){
+    console.log("k is less than t");
+
+}
+if(k>t){
+    console.log("k is greater than t");
+
+}
+function checkpassed(pass,fail){
+    if(pass>fail){
+        console.log("you are passed");
+    }
+
+     else {
+        console.log("you are failed");
+    }
+}
+checkpassed(80,40);
+checkpassed(30,60);
+checkpassed(50,50);
+checkpassed(90,20);
+var k=40;
+var t=50;
+
+    console.log("k is less than t");
+switch(k){
+    case 10:
+        console.log("k is 10");
+        break;
+    case 20:
+        console.log("k is 20");
+        break;
+    case 30:
+        console.log("k is 30");
+        
+
+        }
+        for(var i=1;i<=5;i++){
+            console.log("keno too");
+        }
+        var stud=[1,2,3,4,5];
+        var pass=50;
+        for( i=0;i<=5;i++){
+            console.log(stud[i]);
+            
+
+            }
+            for (var j=1;j<=10;j++){
+                console.log(j);
+            }
+            var keno=[1,2,3,4,5,6,7];
+            var pass=30;
+            for(i=0;i<=6;i++);{
+                console.log(keno[i]);
+            }
+            if(true){
+
+            }
+            var g=10;
+            while(g<=20){
+                console.log(g);
+                g++;
+            }
+
+
+
+
+
